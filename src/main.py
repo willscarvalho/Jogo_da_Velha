9fccedd313d74fab235d7ce5a863b6128de0d048
willscarvalho <will.scarvalho.wsc@gmail.com>
2019-05-24 21:38:17 -0300

    Refatorando codigo, excluindo variaveis desecessarias

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ fn.inicio()
 tabuleiro = fn.criar_tabuleiro()
 jogador1, jogador2 = fn.escolher_jogador()
 fn.mostrar_tabuleiro_inicial()
-jogador = ''
 ganhador = ' '
 fn.jogo_loop(tabuleiro, jogador1, jogador2, ganhador)
 
